d18bcb25221a5a90b54a264048276c35f69cf0fe
dgmccart <92180364+dgmccart@users.noreply.github.com>
2022-10-05 13:03:46 -0700

    Update large_im_test_perf.py

--- large_im_test_perf.py
+++ large_im_test_perf.py
@@ -1,15 +1,40 @@
+#script to access an example of large image data;
+#determine data size (MB, GB, etc.), shape (e.g. 1024 x 1024 x 3), and element type (e.g. uint16);
+#display the image in napari
+
 import fsspec, zarr
 import dask.array as da
 import napari
+import numpy as np
 
-group = zarr.open(zarr.N5FSStore('s3://sci-imaging-data-public/jrc_hela-2/jrc_hela-2.n5/', anon=True)) # access the root of the n5 container
+# access the root of the n5 container
+group = zarr.open(zarr.N5FSStore('s3://sci-imaging-data-public/jrc_hela-2/jrc_hela-2.n5/', anon=True)) 
 
 # s0 (highest resolution) through s5 (lowest resolution) are available,
-# so s3 is a suitable choice in the middle
-zarr_array = group['em/fibsem-uint16/s3']
+zarr_array = group['em/fibsem-uint16/s2']
 data = da.from_zarr(zarr_array, chunks=zarr_array.chunks)
 
-# This order presents a better visualization, but seems to break simple async.
-#viewer = napari.view_image(data, order=(1, 0, 2), contrast_limits=(18000, 40000))
+#calculate and print element type (e.g. uint16), data size (MB, GB, etc.), shape (e.g. 1024 x 1024 x 3)
+num = data.dtype
+print(num)
+
+size = zarr_array.nbytes
+mb = size * (9.5367432 * 10**(-7))
+gb = size * (9.3132 * 10**(-10))
+
+print(size)
+print(mb)
+print(gb)
+
+shape = data.shape
+print(shape)
+
+# Load image into the viewer
 viewer = napari.view_image(data, contrast_limits=(18000, 40000))
 napari.run()
+
+#to run script with performance monitoring use the following command:
+# NAPARI_PERFMON=INSERT_FILE_TREE_TO_JSON_FILE_WITH_FUNCTIONS python INSERT_NAME_OF_THIS_SCRIPT.py
+
+
+

f95ca3d20b7816b718613899cb7a7ba511f0a0a0
dgmccart <92180364+dgmccart@users.noreply.github.com>
2022-10-14 14:37:38 -0700

    Add files via upload

--- test_perf.py
+++ test_perf.py
@@ -1,4 +1,4 @@
-#script to access an example of large image data;
+#script to access an example of small labels data;
 #determine data size (MB, GB, etc.), shape (e.g. 1024 x 1024 x 3), and element type (e.g. uint16);
 #display the image in napari
 
@@ -9,19 +9,32 @@ import os
 import numpy as np
 
 ##block to be modified to access different data
-# access the root of the n5 container
-group = zarr.open(zarr.N5FSStore('s3://sci-imaging-data-public/jrc_hela-2/jrc_hela-2.n5/', anon=True)) 
+import pooch
+from tifffile import imread
 
-# s0 (highest resolution) through s5 (lowest resolution) are available,
-zarr_array = group['em/fibsem-uint16/s2']
-data = da.from_zarr(zarr_array, chunks=zarr_array.chunks)
+"""
+This data comes from the MitoNet Benchmarks.
+Six benchmark volumes of instance segmentation of mitochondria from diverse volume EM datasets
+Narayan K , Conrad RW
+DOI: https://dx.doi.org/10.6019/EMPIAR-10982
+Data is stored at EMPIAR and can be explored here: https://www.ebi.ac.uk/empiar/EMPIAR-10982/
+With respect to the napari async slicing work, this dataset is small enough that it performs well in synchronous mode.
+"""
+
+salivary_gland_mito_path = pooch.retrieve(
+    url='https://ftp.ebi.ac.uk/empiar/world_availability/10982/data/mito_benchmarks/salivary_gland/salivary_gland_mito.tif',
+    known_hash='95247d952a1dd0f7b37da1be95980b598b590e4777065c7cd877ab67cb63c5eb',
+)
+
+salivary_gland_mito = imread(salivary_gland_mito_path)
+
 ##end of block to be modified to access different data
 
 #calculate and print element type (e.g. uint16), data size (MB, GB, etc.), shape (e.g. 1024 x 1024 x 3)
-num = data.dtype
+num = salivary_gland_mito.dtype
 print(num)
 
-size = zarr_array.nbytes
+size = salivary_gland_mito.nbytes
 mb = size * (9.5367432 * 10**(-7))
 gb = size * (9.3132 * 10**(-10))
 
@@ -29,14 +42,9 @@ print(size)
 print(mb)
 print(gb)
 
-shape = data.shape
+shape = salivary_gland_mito.shape
 print(shape)
 
-# Load image into the viewer
-viewer = napari.view_image(data, contrast_limits=(18000, 40000))
+# Load image into the viewer [EDIT FOR NEW DATA]
+viewer = napari.view_image(salivary_gland_mito)
 napari.run()
-
-#to run script with performance monitoring use the following command:
-# NAPARI_PERFMON=INSERT_FILE_TREE_TO_JSON_FILE_WITH_FUNCTIONS python INSERT_NAME_OF_THIS_SCRIPT.py
-
-
